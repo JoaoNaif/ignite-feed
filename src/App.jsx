ee5f3b271c2742eb90eb463893f96cae9e4c0000
João Victor Naif De Abreu <joaonaif@localhost.localdomain>
2024-09-10 00:29:02 -0300

    initial project feed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header'
+import './styles.css'
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Vai Corinthians</h1>
+        <Header/>
+      </div>
+    </>
+  )
+}
+
+export default App
